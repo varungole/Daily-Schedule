c52e93bf71911adbd170b23812a1afff5bfd8e2a
Varun Gole <varungole1999@gmail.com>
2024-04-05 16:46:03 -0400

    working on user service

---
+++ frontend/src/services/user.service.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = '';
+
+class UserService {
+
+    getPublicContent() {
+
+    }
+
+    getUserBoard() {
+
+    }
+
+    getModeratorBoard() {
+
+    }
+
+    getAdminBoard() {
+
+    }
+}
+
+export default new UserService();
