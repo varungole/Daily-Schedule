54f5d9807f2c4444d5767a5d9ac14c952d0a4051
Varun Gole <varungole1999@gmail.com>
2024-04-06 13:43:47 -0400

    commit

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -6,19 +6,19 @@ const API_URL = '';
 class UserService {
 
     getPublicContent() {
-
+        return axios.get(API_URL + 'all');
     }
 
     getUserBoard() {
-
+        return axios.get(API_URL + 'user', {headers: authHeader()});
     }
 
     getModeratorBoard() {
-
+        return axios.get(API_URL + 'mod' , {headers: authHeader()});
     }
 
     getAdminBoard() {
-
+        return axios.get(API_URL + 'admin', {headers: authHeader()});
     }
 }
 
