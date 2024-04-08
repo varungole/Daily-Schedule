697cf8d7cc3b2ec38b18eb91ef92963dea4dba7c
Varun Gole <varungole1999@gmail.com>
2024-04-08 17:11:05 -0400

    commit

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -1,5 +1,46 @@
-import Form from 'react-validation'
-import Input from 'react-validation'
-import CheckButton from 'react-validation'
+import React, {Component} from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
 
-import {isEmail} from "validator" //to verify email
+import authService from "../services/auth.service";
+
+import { withRouter } from "../commons/with-router";
+
+const required = value => {
+    if(!value)
+    {
+        return (
+            <div className="alert-danger" role="alert">
+                This field is Required!
+            </div>
+        )
+    }
+}
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+
+        this.state = {
+            username: "",
+            password: "",
+            loading: false,
+            message: ""
+        };
+    }
+
+
+    onChangePassword(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangePassword(e) {
+        
+    }
+}
